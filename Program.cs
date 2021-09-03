43ff86309eae86e5fcdc03c94763f4f6d09c1803
Raylla-Silva12 <raylla.l.silva@gmail.com>
2021-09-03 14:28:29 -0300

    C# cod_3

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace l_repeticao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("\nDesenvolva um programa que mostre na tela uma tabuada escolhida pelo usuário.\n");
+            Console.Write("Digite um número: ");
+            num = int.Parse(Console.ReadLine());
+
+            for (int multiplicando = 0; multiplicando <= 10; multiplicando++)
+            {
+                Console.WriteLine(num + " x " + multiplicando + " = " + (num * multiplicando));
+            }
+            Console.ReadKey();
+        }
+    }
+}
